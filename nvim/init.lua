8cc3068fd2b91bd35b105de53f07427959eb7449
Mario Guillen <marioagr95@gmail.com>
2024-02-16 19:15:04 -0600

    Init

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+-- Set leader key
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-----------------------
+
+require('marrio/keymaps')
---
+++ nvim/lua/marrio/keymaps.lua
@@ -0,0 +1,38 @@
+--[[
+    "Native" keymaps for neovim
+    NO PLUGINS
+--]]
+
+-- When text is wrapped, move by terminal rows instead, not lines, unless a count is provided.
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Reselect visual selection after indenting.
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Maintain the cursor position when yanking a visual selection.
+-- [M]ark at [y] position then use [`y] and go to that mark
+vim.keymap.set('v', 'y', 'myy`y')
+
+-- Disable annoying command line typo.
+-- vim.keymap.set('n', 'q:', 'q:')
+
+-- Paste replace visual selection without copying it.
+-- ["] choose the [_] register (which acts like a black-hole) then [d]elete it and [P]aste it
+vim.keymap.set('v', 'p', '"_dP')
+
+-- Easy insertion of a trailing ; or , from insert mode.
+vim.keymap.set('i', ';;', '<Esc>A;')
+vim.keymap.set('i', ',,', '<Esc>A,')
+
+-- Quickly clear search highlighting.
+vim.keymap.set('n', '<Leader>k', ':nohlsearch<CR>')
+
+-- Move lines up and down using Alt+[j|k].
+vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('n', '<A-j>', ':move .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':move .-2<CR>==')
+vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
