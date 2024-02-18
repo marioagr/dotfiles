1c9f6dda4b113ce93e2e0ae2e2b0e50a911ea2b2
Mario Guillen <marioagr95@gmail.com>
2024-02-18 13:53:16 -0600

    Added bufferline-nvim

---
+++ nvim/lua/plugins/bufferline-nvim.lua
@@ -0,0 +1,36 @@
+-- Display buffers as tabs
+return {
+    'akinsho/bufferline.nvim',
+    dependencies = {
+        'kyazdani42/nvim-web-devicons',
+    },
+    after = 'onedark.nvim',
+    opts = {
+        options = {
+            show_close_item = false,
+            max_name_length = 25,
+            numbers = 'ordinal',
+            separator_style = 'slope',
+            offsets = {
+                {
+                    filetype = 'NvimTree',
+                    text = ' Files',
+                    highlight = 'StatusLine',
+                    text_align = 'left',
+                }
+            },
+            custom_areas = {
+                left = function()
+                    return {
+                        { text = '  ', fg = '#3e93d3' },
+                    }
+                end,
+            },
+            hover = {
+                enabled = true,
+                delay = 200,
+                reveal = { 'close' }
+            },
+        }
+    },
+}
