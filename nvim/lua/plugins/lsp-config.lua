583e5b2a1e357fedfe65e909454f66931b12b417
Mario Guillen <marioagr95@gmail.com>
2024-02-23 01:10:37 -0600

    Added nvim-cmp and dependencies

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -115,8 +115,7 @@ return {
             require('neodev').setup()
             -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
             local capabilities = vim.lsp.protocol.make_client_capabilities()
-            -- TODO Enable this after cmp installed
-            -- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
             -- Ensure the servers above are installed
             local mason_lspconfig = require('mason-lspconfig')
