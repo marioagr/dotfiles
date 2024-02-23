6015df98f4be444849c0e879462eb1843a542fb7
Mario Guillen <marioagr95@gmail.com>
2024-02-22 22:03:10 -0600

    Added none-ls

---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,21 @@
+return {
+    'nvimtools/none-ls.nvim',
+    config = function()
+        local null_ls = require('null-ls')
+        null_ls.setup({
+            sources = {
+                --[[
+                    DO NOT USE ANY BUILTINS THAT ARE CONFIGURED IN
+                    nvim/lua/plugins/lsp-config.lua
+
+                    For example: Having lua_ls in lsp-config and
+                    having enabled [null_ls.builtins.formatting.stylua]
+                    runs double formatting
+                        First the one in lsp-config.lua loading config options
+                        Then the one in this file ignoring the previous config options.
+                --]]
+                null_ls.builtins.diagnostics.php,
+            },
+        })
+    end,
+}
