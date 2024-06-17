c1c14016bcc53e774f2b2c0046decdfebbc418ba
Mario Guillen <marioagr95@gmail.com>
2024-06-17 14:13:57 -0600

    Additions to tree-sitter-blade plugin

--- nvim/queries/blade/injections.scm
+++ nvim/queries/blade/injections.scm
@@ -1,17 +1,21 @@
+; Inject HTML/PHP
 ((text) @injection.content
     (#not-has-ancestor? @injection.content "envoy")
     (#set! injection.combined)
     (#set! injection.language php))
 
-((text) @injection.content
-    (#has-ancestor? @injection.content "envoy")
-    (#set! injection.combined)
-    (#set! injection.language bash))
+; Inject Envoy/Bash
+; ((text) @injection.content
+;     (#has-ancestor? @injection.content "envoy")
+;     (#set! injection.combined)
+;     (#set! injection.language bash))
 
-
+; Inject php_only
 ((php_only) @injection.content
     (#set! injection.combined)
     (#set! injection.language php_only))
+
+; Inject parameter
 ((parameter) @injection.content
     (#set! injection.language php_only))
 
