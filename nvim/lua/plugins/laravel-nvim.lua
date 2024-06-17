3fe454b950152a84a4e68f4bb3a8a78d4f67a6c6
Mario Guillen <marioagr95@gmail.com>
2024-06-17 12:32:03 -0600

    Plugin to ease "DX" related to Laravel projects

---
+++ nvim/lua/plugins/laravel-nvim.lua
@@ -0,0 +1,21 @@
+return {
+    'adalessa/laravel.nvim',
+    dependencies = {
+        'nvim-telescope/telescope.nvim',
+        'tpope/vim-dotenv',
+        'MunifTanjim/nui.nvim',
+        'nvimtools/none-ls.nvim',
+    },
+    cmd = { 'Sail', 'Artisan', 'Composer', 'Npm', 'Yarn', 'Laravel' },
+    keys = {
+        { '<leader>la', ':Laravel artisan<cr>' },
+        { '<leader>lr', ':Laravel routes<cr>' },
+        { '<leader>lm', ':Laravel related<cr>' },
+        { '<leader>lsu', ':Sail up -d<cr>' },
+        { '<leader>lsd', ':Sail down<cr>' },
+    },
+    event = { 'VeryLazy' },
+    opts = {
+        lsp_server = 'intelephense',
+    },
+}
