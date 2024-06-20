6e0dc665c9bce611ef6a23c7c7b4ba27981272ee
Mario Guillen <marioagr95@gmail.com>
2024-06-19 20:09:10 -0600

    Added new keybind and formatted filed

--- nvim/lua/plugins/git-signs-nvim.lua
+++ nvim/lua/plugins/git-signs-nvim.lua
@@ -42,13 +42,15 @@ return {
             end, { expr = true, desc = 'Jump to previous hunk' })
 
             -- Actions
+
             -- visual mode
             map('v', '<leader>hs', function()
-                gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+                gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
             end, { desc = 'stage git hunk' })
             map('v', '<leader>hr', function()
-                gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+                gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
             end, { desc = 'reset git hunk' })
+
             -- normal mode
             map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
             map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
@@ -57,16 +59,17 @@ return {
             map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
             map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
             map('n', '<leader>hb', function()
-                gs.blame_line { full = false }
+                gs.blame_line({ full = false })
             end, { desc = 'git blame line' })
             map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
             map('n', '<leader>hD', function()
-                gs.diffthis '~'
+                gs.diffthis('~')
             end, { desc = 'git diff against last commit' })
 
             -- Toggles
-            map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-            map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+            map('n', '<leader>gsl', ':Gitsigns toggle_linehl<CR>', { desc = '[g]it[s]igns show [l]ine highlight' })
+            map('n', '<leader>gsb', gs.toggle_current_line_blame, { desc = '[g]it[s]igns [b]lame line' })
+            map('n', '<leader>gsd', gs.toggle_deleted, { desc = '[g]it[s]igns show [d]eleted' })
 
             -- Text object
             map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
