56f548b5149db000dc4e3f08b94dcf3bd82f0ee6
Mario Guillen <marioagr95@gmail.com>
2024-07-13 20:03:57 -0600

    Remap some cmp mappings and added buffer plugin

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -22,6 +22,7 @@ return {
         'saadparwaiz1/cmp_luasnip',
 
         -- Adds LSP completion capabilities
+        'hrsh7th/cmp-buffer', -- source for buffer words
         'hrsh7th/cmp-nvim-lsp', -- For Neovim's built-in language server client
         'hrsh7th/cmp-path', -- For filesystem paths
         'hrsh7th/cmp-nvim-lsp-signature-help', -- For displaying function signatures with the current parameter emphasized
@@ -60,6 +61,7 @@ return {
                         completeopt = 'menu,menuone,preview',
                 --]]
             },
+            ---@diagnostic disable-next-line: missing-fields
             formatting = {
                 format = lspkind.cmp_format({
                     mode = 'symbol_text',
@@ -68,10 +70,10 @@ return {
             },
             mapping = cmp.mapping.preset.insert({
                 -- Select the [n]ext item
-                ['<C-n>'] = cmp.mapping.select_next_item(),
+                ['<C-Down>'] = cmp.mapping.select_next_item(),
 
                 -- Select the [p]revious item
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<C-Up>'] = cmp.mapping.select_prev_item(),
 
                 -- Move docs [f]orward
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -84,36 +86,35 @@ return {
                 -- Accept ([y]es) the completion.
                 --  This will auto-import if your LSP supports it.
                 --  This will expand snippets if the LSP sent a snippet.
-                ['<C-y>'] = cmp.mapping.confirm(),
+                ['<C-Right>'] = cmp.mapping.confirm(),
 
                 -- Manually trigger a completion from nvim-cmp.
                 --  Generally you don't need this, because nvim-cmp will display
                 --  completions whenever it has completion options available.
                 ['<C-Space>'] = cmp.mapping.complete(),
 
-                -- Think of <c-l> as moving to the right of your snippet expansion.
+                -- Think of <M-Right> as moving to the right of your snippet expansion.
                 --  So if you have a snippet that's like:
                 --  function $name($args)
                 --    $body
                 --  end
                 --
-                -- <c-l> will move you to the right of each of the expansion locations.
-                -- <c-h> is similar, except moving you backwards.
-                ['<C-l>'] = cmp.mapping(function()
+                -- <M-Right> will move you to the right of each of the expansion locations.
+                -- <M-Left> is similar, except moving you backwards.
+                ['<M-Right>'] = cmp.mapping(function()
                     if luasnip.expand_or_locally_jumpable() then
                         luasnip.expand_or_jump()
                     end
                 end, { 'i', 's' }),
-                ['<C-h>'] = cmp.mapping(function()
+                ['<M-Left>'] = cmp.mapping(function()
                     if luasnip.locally_jumpable(-1) then
                         luasnip.jump(-1)
                     end
                 end, { 'i', 's' }),
             }),
-            sources = {
+            sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' },
-                { name = 'path' },
                 { name = 'nvim_lsp_signature_help' },
                 {
                     name = 'omni',
@@ -121,7 +122,10 @@ return {
                         disable_omnifuncs = { 'v:lua.vim.lsp.omnifunc' },
                     },
                 },
-            },
+            }, {
+                { name = 'path' },
+                { name = 'buffer' },
+            }),
         })
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
