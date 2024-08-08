7d7e767f5dd478154e3c4b6e0f9006a56d1d44ca
Mario Guillen <marioagr95@gmail.com>
2024-08-07 19:17:25 -0600

    Added a plugin that adds text objects for HTML/XML attributes

---
+++ nvim/lua/plugins/vim-textobj-xmlattr.lua
@@ -0,0 +1,8 @@
+-- Text objects for HTML attributes.
+return {
+    -- NOTE: Adds ax/ix text objects for HTML/XML attributes
+    'whatyouhide/vim-textobj-xmlattr',
+    dependencies = {
+        'kana/vim-textobj-user',
+    },
+}
