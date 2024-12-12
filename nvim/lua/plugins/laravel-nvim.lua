31d0dc1b39713c83a49e341beaced5c3568d5a83
Mario Guillen <marioagr95@gmail.com>
2024-12-11 19:57:27 -0600

    Update keymaps and config of laravel.nvim

--- nvim/lua/plugins/laravel-nvim.lua
+++ nvim/lua/plugins/laravel-nvim.lua
@@ -8,19 +8,15 @@ return {
         -- 'nvimtools/none-ls.nvim',
         'kevinhwang91/promise-async',
     },
-    cmd = { 'Sail', 'Artisan', 'Composer', 'Npm', 'Yarn', 'Laravel' },
+    cmd = { 'Laravel' },
     keys = {
         { '<leader>la', ':Laravel artisan<cr>', desc = 'Open Laravel Artisan menu' },
+        { '<leader>lc', ':Laravel commands<cr>', desc = 'Execute a (user defined) command' },
+        { '<leader>lf', ':Laravel related<cr>', desc = 'Show Laravel related files' },
         { '<leader>lh', ':Laravel history<cr>', desc = 'Show history of Laravel commands' },
-        { '<leader>lm', ':Laravel related<cr>', desc = 'Show Laravel related files' },
+        { '<leader>lm', ':Laravel make<cr>', desc = 'Available content to [m]ake' },
         { '<leader>lr', ':Laravel routes<cr>', desc = 'Show current routes of the project' },
-        { '<leader>lst', ':Sail shell<cr>', desc = 'Start a terminal inside the container' },
-        { '<leader>lsu', ':Sail up -d<cr>', desc = 'Start Sail dettached' },
-        { '<leader>lsr', ':Sail restart<cr>', desc = 'Restart Sail' },
-        { '<leader>lsd', ':Sail down<cr>', desc = 'Stop Sail' },
-        { '<leader>lss', ':Sail ps<cr>', desc = 'Status of running containers' },
-        { '<leader>Nd', ':Npm dev<cr>', desc = 'Run [N]pm [d]ev' },
-        { '<leader>Nb', ':Npm build<cr>', desc = 'Run [N]pm [b]uild' },
+        { '<leader>lR', ':Laravel resources<cr>', desc = 'Go to a resource of the project' },
     },
     event = { 'VeryLazy' },
     opts = {
@@ -34,6 +30,44 @@ return {
                     },
                 },
                 -- Maybe set the same window option for the popup?
+            },
+        },
+        user_commands = {
+            artisan = {
+                ['db:fresh'] = {
+                    cmd = { 'migrate:fresh', '--seed' },
+                    desc = "Re-creates the db and seed's it",
+                },
+            },
+            npm = {
+                build = {
+                    cmd = { 'run', 'build' },
+                    desc = 'Builds the javascript assets',
+                },
+                dev = {
+                    cmd = { 'run', 'dev' },
+                    desc = 'Builds the javascript assets',
+                },
+            },
+            composer = {
+                autoload = {
+                    cmd = { 'dump-autoload' },
+                    desc = 'Dumps the composer autoload',
+                },
+            },
+            sail = {
+                start = {
+                    cmd = { 'up', '-d' },
+                    desc = 'Start Sail dettached',
+                },
+                stop = {
+                    cmd = { 'stop' },
+                    desc = 'Stop Sail',
+                },
+                build_no_cache = {
+                    cmd = { 'build', '--no-cache' },
+                    desc = 'Build Sail with no cache',
+                },
             },
         },
     },
