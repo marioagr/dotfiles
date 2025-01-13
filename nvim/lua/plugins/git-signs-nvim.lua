bb09d436bac9e8362d20ac5ded79912abb3306f3
Mario Guillen <marioagr95@gmail.com>
2025-01-13 14:38:51 -0600

    Update

--- nvim/lua/plugins/git-signs-nvim.lua
+++ nvim/lua/plugins/git-signs-nvim.lua
@@ -18,7 +18,7 @@ return {
                 --     return ']c'
                 -- end
                 vim.schedule(function()
-                    gs.next_hunk()
+                    gs.nav_hunk('next')
                 end)
                 return '<Ignore>'
             end, { expr = true, desc = 'Jump to next hunk' })
@@ -28,7 +28,7 @@ return {
                 --     return '[c'
                 -- end
                 vim.schedule(function()
-                    gs.prev_hunk()
+                    gs.nav_hunk('prev')
                 end)
                 return '<Ignore>'
             end, { expr = true, desc = 'Jump to previous hunk' })
