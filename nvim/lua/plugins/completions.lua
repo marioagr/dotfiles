6090cca6338e2273dbc51119b2b4c7a1507b8282
Mario Guillen <marioagr95@gmail.com>
2025-04-20 11:07:22 -0600

    Now the snippets are working properly

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -9,10 +9,8 @@ return {
             'L3MON4D3/LuaSnip',
             version = '2.*',
             build = 'make install_jsregexp',
-            config = function()
-                require('luasnip.loaders.from_vscode').lazy_load()
-            end,
         },
+        'raifiamadriz/friendly-snippets',
         {
             'xzbdmw/colorful-menu.nvim',
             opts = {},
@@ -21,121 +19,115 @@ return {
         'onsails/lspkind.nvim',
         'folke/lazydev.nvim',
     },
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        keymap = {
-            preset = 'default',
-            -- Override default
-            ['<Up>'] = {},
-            ['<Down>'] = {},
+    config = function()
+        require('blink-cmp').setup({
+            keymap = {
+                preset = 'default',
+                -- <C-space> 'show',
+                ['<C-e>'] = { 'hide', 'fallback' },
+                -- <ctrl-y> select_and_accept
+                ['<M-CR>'] = { 'select_and_accept', 'fallback' },
 
-            ['<C-Up>'] = { 'select_prev' },
-            ['<C-Down>'] = { 'select_next' },
+                ['<Up>'] = {},
+                ['<Down>'] = {},
 
-            ['<C-k>'] = {},
-            ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
+                ['<C-Up>'] = { 'select_prev' },
+                ['<C-Down>'] = { 'select_next' },
 
-            ['<C-y>'] = {},
-            ['<C-Right>'] = { 'accept', 'fallback' },
-        },
+                ['<C-k>'] = {},
+                ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
 
-        appearance = {
-            nerd_font_variant = 'mono',
-        },
-
-        completion = {
-            accept = {
-                -- How long to wait for the LSP to resolve the item with additional information before continuing as-is
-                resolve_timeout_ms = 250,
+                ['<M-d>'] = { 'show_documentation', 'hide_documentation' },
             },
-            documentation = {
-                auto_show = false,
-                auto_show_delay_ms = 1000,
+            appearance = {
+                nerd_font_variant = 'mono',
             },
-            menu = {
-                border = 'rounded',
-                draw = {
-                    -- Components to render, grouped by column
-                    padding = { 0, 1 },
-                    columns = { { 'kind_icon' }, { 'label', gap = 1 }, { 'source_name' } },
-                    components = {
-                        kind_icon = {
-                            text = function(ctx)
-                                local lspkind = require('lspkind')
-                                local icon = ctx.kind_icon
-                                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
-                                    local dev_icon, _ = require('nvim-web-devicons').get_icon(ctx.label)
-                                    if dev_icon then
-                                        icon = dev_icon
-                                    end
-                                else
-                                    icon = lspkind.symbolic(ctx.kind, { mode = 'symbol' })
-                                end
-
-                                return icon .. ctx.icon_gap
-                            end,
-                            highlight = function(ctx)
-                                local hl = 'BlinkCmpKind' .. ctx.kind or require('blink.cmp.completion.windows.render.tailwind').get_hl(ctx)
-                                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
-                                    local dev_icon, dev_hl = require('nvim-web-devicons').get_icon(ctx.label)
-                                    if dev_icon then
-                                        hl = dev_hl
-                                    end
-                                end
-                                return hl
-                            end,
-                        },
-                        label = {
-                            text = function(ctx)
-                                return require('colorful-menu').blink_components_text(ctx)
-                            end,
-                            highlight = function(ctx)
-                                return require('colorful-menu').blink_components_highlight(ctx)
-                            end,
-                        },
+            completion = {
+                accept = {
+                    -- How long to wait for the LSP to resolve the item with additional information before continuing as-is
+                    resolve_timeout_ms = 250,
+                },
+                documentation = {
+                    auto_show = false,
+                    auto_show_delay_ms = 1000,
+                },
+                list = {
+                    selection = {
+                        preselect = false,
                     },
-                    -- Use treesitter to highlight the label text for the given list of sources
-                    treesitter = { 'lsp' },
+                },
+                menu = {
+                    border = 'rounded',
+                    draw = {
+                        -- Components to render, grouped by column
+                        padding = { 0, 1 },
+                        columns = { { 'kind_icon' }, { 'label', gap = 1 }, { 'source_name' } },
+                        components = {
+                            kind_icon = {
+                                text = function(ctx)
+                                    local lspkind = require('lspkind')
+                                    local icon = ctx.kind_icon
+                                    if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                                        local dev_icon, _ = require('nvim-web-devicons').get_icon(ctx.label)
+                                        if dev_icon then
+                                            icon = dev_icon
+                                        end
+                                    else
+                                        icon = lspkind.symbolic(ctx.kind, { mode = 'symbol' })
+                                    end
+
+                                    return icon .. ctx.icon_gap
+                                end,
+                                highlight = function(ctx)
+                                    local hl = 'BlinkCmpKind' .. ctx.kind or require('blink.cmp.completion.windows.render.tailwind').get_hl(ctx)
+                                    if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                                        local dev_icon, dev_hl = require('nvim-web-devicons').get_icon(ctx.label)
+                                        if dev_icon then
+                                            hl = dev_hl
+                                        end
+                                    end
+                                    return hl
+                                end,
+                            },
+                            label = {
+                                text = function(ctx)
+                                    return require('colorful-menu').blink_components_text(ctx)
+                                end,
+                                highlight = function(ctx)
+                                    return require('colorful-menu').blink_components_highlight(ctx)
+                                end,
+                            },
+                        },
+                        -- Use treesitter to highlight the label text for the given list of sources
+                        treesitter = { 'lsp' },
+                    },
                 },
             },
-        },
-
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        sources = {
-            default = { 'lsp', 'buffer', 'snippets', 'path', 'lazydev' },
-            per_filetype = { sql = { 'dadbod' } },
-            providers = {
-                dadbod = { module = 'vim_dadbod_completion.blink' },
-                lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+            sources = {
+                default = { 'lsp', 'buffer', 'snippets', 'path', 'lazydev' },
+                per_filetype = { sql = { 'dadbod' } },
+                providers = {
+                    dadbod = { module = 'vim_dadbod_completion.blink' },
+                    lazydev = { module = 'lazydev.integrations.blink' },
+                },
             },
-        },
-
-        snippets = { preset = 'luasnip' },
-
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-        --
-        -- See the fuzzy documentation for more information
-        fuzzy = {
-            implementation = 'prefer_rust_with_warning',
-            sorts = {
-                'exact',
-                'score',
-                'sort_text',
+            fuzzy = {
+                implementation = 'prefer_rust_with_warning',
+                sorts = {
+                    'exact',
+                    'score',
+                    'sort_text',
+                },
             },
-        },
-
-        -- Shows a signature help window while you type arguments for a function
-        signature = {
-            enabled = true,
-            window = {
-                -- TODO: Try this
-                show_documentation = false,
+            snippets = { preset = 'luasnip' },
+            signature = {
+                enabled = true,
+                window = {
+                    show_documentation = false,
+                },
             },
-        },
-    },
-    opts_extend = { 'sources.default' },
+        })
+
+        require('luasnip.loaders.from_vscode').lazy_load()
+    end,
 }
