cf9eef9528ed7d76e592ebb8fdf5b6cc301005eb
Mario Guillen <marioagr95@gmail.com>
2025-05-01 23:17:29 -0600

    Update plugins and restore phpactor

--- nvim/lua/plugins/phpactor.lua
+++ nvim/lua/plugins/phpactor.lua
@@ -2,8 +2,7 @@ return {
     'phpactor/phpactor',
     ft = 'php',
     build = 'composer install --no-dev --optimize-autoloader',
-    opts = {},
-    keys = {
-        { '<leader>pm', ':PhpactorContextMenu<CR>', desc = '[p]hpactor [m]enu' },
-    },
+    config = function()
+        __setKeymap('<leader>pm', ':PhpactorContextMenu<CR>', { desc = '[p]hpactor' })
+    end,
 }
