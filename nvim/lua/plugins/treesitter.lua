f0541fc7e9698c68b679f220ff06aa978381ca19
Mario Guillen <marioagr95@gmail.com>
2025-06-16 11:14:31 -0600

    ...

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -53,8 +53,9 @@ return {
             modules = {},
             highlight = {
                 enable = true,
+                -- Disable in files greater than 2.5 MB
                 disable = function(lang, buf)
-                    local max_filesize = 1024 * 2500 -- 2.5 MB
+                    local max_filesize = 2.5 * 1024 * 1024 -- M * K * B
                     local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
                     if ok and stats and stats.size > max_filesize then
                         return true
