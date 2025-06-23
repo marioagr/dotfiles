4f53068d0c4d4208f38889bb4df83187eb65b4db
Mario Guillen <marioagr95@gmail.com>
2025-06-23 10:07:37 -0600

    ...

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -128,10 +128,18 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_wid
 
     local edge_foreground = background
 
-    local title = string.format('%s| %s', tab.tab_index + 1, get_tab_title(tab))
+    local title = get_tab_title(tab)
+
+    if title == 'nvim' or title == 'neovim' then
+        title = ' ' .. title
+    elseif title == 'lazygit' or title == 'git' then
+        title = '󰊢 ' .. title
+    end
+
+    title = string.format('%s: %s', tab.tab_index + 1, title)
 
     if tab.active_pane.is_zoomed then
-        title = string.format('%s %s', EXPANDED_PANE, title)
+        title = title:gsub(':', ' ' .. EXPANDED_PANE .. ' ')
     end
 
     title = wezterm.truncate_right(title, max_width - 2)
