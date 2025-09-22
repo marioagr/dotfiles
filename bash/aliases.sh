ce235e39019a612c900666a784d8b99e88d6c69e
Mario Guillen <marioagr95@gmail.com>
2025-09-21 22:05:34 -0600

    ...

---
+++ bash/aliases.sh
@@ -0,0 +1,15 @@
+alias cat='batcat'
+
+alias artisan='php artisan'
+alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+alias supd='sail up -d && sail pnpm run dev'
+
+alias prd='pnpm run dev'
+alias prb='pnpm run build'
+
+alias lzg='lazygit'
+
+alias lzd='lazydocker'
+
+alias doc="docker-compose"
+
