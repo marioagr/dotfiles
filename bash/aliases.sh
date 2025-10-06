0da72434f112b3a3b58bc53d27f70e1a96be27c0
Mario Guillen <marioagr95@gmail.com>
2025-10-06 16:34:21 -0600

    ...

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,8 +2,14 @@ alias cat='batcat'
 
 alias artisan='php artisan'
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+
+alias sund='sail up -d && sail npm run dev'
 alias supd='sail up -d && sail pnpm run dev'
 
+# NPM
+alias nrd='npm run dev'
+alias nrb='npm run build'
+# PNPM
 alias prd='pnpm run dev'
 alias prb='pnpm run build'
 
