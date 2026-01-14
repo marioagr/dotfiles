661c1940a9bcb9b4bdfafb5f1acd74491e5dc2ad
Mario Guillen <marioagr95@gmail.com>
2026-01-14 15:33:12 -0600

    Use LSP protocol strings

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -138,7 +138,7 @@ return {
                 --
                 -- When you move your cursor, the highlights will be cleared (the second autocommand).
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
-                -- if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+                -- if client and client:supports_method('textDocument/documentHighlight', event.buf) then
                 --     local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
                 --     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                 --         buffer = event.buf,
@@ -165,7 +165,7 @@ return {
                 -- code, if the language server you are using supports them
                 --
                 -- This may be unwanted, since they displace some of your code
-                if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+                if client and client:supports_method('textDocument/inlayHint', event.buf) then
                     map('<leader>th', function()
                         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
                     end, 'inlay [h]ints')
@@ -189,16 +189,8 @@ return {
             virtual_text = {
                 source = 'if_many',
                 spacing = 2,
-                format = function(diagnostic)
-                    local diagnostic_message = {
-                        [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                        [vim.diagnostic.severity.WARN] = diagnostic.message,
-                        [vim.diagnostic.severity.INFO] = diagnostic.message,
-                        [vim.diagnostic.severity.HINT] = diagnostic.message,
-                    }
-                    return diagnostic_message[diagnostic.severity]
-                end,
             },
+            -- virtual_lines = true,
         })
 
         -- LSP servers and clients are able to communicate to each other what features they support.
