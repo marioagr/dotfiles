05bd2ba43d2cd3567eba46f28c7dc5a0e75925ca
Mario Guillen <marioagr95@gmail.com>
2026-01-17 10:46:38 -0600

    Disable phpactor tmp

--- nvim/lua/plugins/phpactor.lua
+++ nvim/lua/plugins/phpactor.lua
@@ -1,5 +1,6 @@
 return {
     'phpactor/phpactor',
+    enabled = false,
     ft = 'php',
     build = 'composer install --no-dev --optimize-autoloader',
     config = function() __setKeymap('<leader>pm', ':PhpactorContextMenu<CR>', { desc = '[p]hpactor' }) end,
