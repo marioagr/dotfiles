f5775c4b0d1cdad9f66505f6a975645f334524d7
Mario Guillen <marioagr95@gmail.com>
2026-03-14 12:43:07 -0600

    Add keybind for right click

--- wezterm/my_keys.lua
+++ wezterm/my_keys.lua
@@ -329,6 +329,22 @@ local key_tables = {
     },
 }
 
+local mouse_binds = {
+    {
+        event = { Down = { streak = 1, button = 'Right' } },
+        mods = 'NONE',
+        action = wezterm.action_callback(function(window, pane)
+            local has_selection = window:get_selection_text_for_pane(pane) ~= ''
+            if has_selection then
+                window:perform_action(act.CopyTo('ClipboardAndPrimarySelection'), pane)
+                window:perform_action(act.ClearSelection, pane)
+            else
+                window:perform_action(act({ PasteFrom = 'Clipboard' }), pane)
+            end
+        end),
+    },
+}
+
 -- define a function in the module table.
 -- Only functions defined in `module` will be exported to
 -- code that imports this module.
@@ -340,6 +356,7 @@ function M.apply_to_config(config)
     config.leader = leader_key
     config.keys = keys
     config.key_tables = key_tables
+    config.mouse_bindings = mouse_binds
 end
 
 return M
